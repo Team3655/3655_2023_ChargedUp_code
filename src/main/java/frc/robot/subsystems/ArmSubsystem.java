820afdcfcff87d4fbaf2bdde25ed33b55d615b12
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2023-01-26 10:37:16 -0500

    Started work on position pid

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,10 +3,27 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class ArmSubsystem extends SubsystemBase {
 
 	// region properties
+
+	// the motor objects for the motors controling the arms
+	private CANSparkMax rightMajorMotor;
+	private CANSparkMax leftMajorMotor;
+	private CANSparkMax rightMinorMotor;
+	private CANSparkMax leftMinorMotor;
+
+	// the pid controlers for the major and minor arms
+	private SparkMaxPIDController majorPIDController;
+	private SparkMaxPIDController minorPIDController;
+
+	private RelativeEncoder rightMajorEncoder;
+	private RelativeEncoder rightMinorEncoder;
 
 	/** used for controling the height of the arm */
 	private enum ArmPoses {
@@ -35,9 +52,67 @@ public class ArmSubsystem extends SubsystemBase {
 	// endregion
 
 	public ArmSubsystem() {
+
+		// the default state of the arms
 		isTucked = true;
 		isFront = true;
 		armState = ArmPoses.LOW_INTAKE;
+
+		// creates the arms motors
+		rightMajorMotor = new CANSparkMax(ArmConstants.kRightMajorArmPort, MotorType.kBrushless);
+		leftMajorMotor = new CANSparkMax(ArmConstants.kLeftMajorArmPort, MotorType.kBrushless);
+		rightMinorMotor = new CANSparkMax(ArmConstants.kRightMinorArmPort, MotorType.kBrushless);
+		leftMinorMotor = new CANSparkMax(ArmConstants.kLeftMinorArmPort, MotorType.kBrushless);
+
+		/**
+		 * The restoreFactoryDefaults method can be used to reset the configuration
+		 * parameters in the SPARK MAX to their factory default state. If no argument is
+		 * passed, these parameters will not persist between power cycles
+		 */
+		rightMajorMotor.restoreFactoryDefaults();
+		leftMajorMotor.restoreFactoryDefaults();
+		rightMinorMotor.restoreFactoryDefaults();
+		leftMinorMotor.restoreFactoryDefaults();
+
+		/**
+		 * In order to use PID functionality for a controller, a SparkMaxPIDController
+		 * object is constructed by calling the getPIDController() method on an existing
+		 * CANSparkMax object
+		 */
+		majorPIDController = rightMajorMotor.getPIDController();
+		minorPIDController = rightMajorMotor.getPIDController();
+
+		// Encoder object created to display position values
+		rightMajorEncoder = rightMajorMotor.getEncoder();
+		rightMinorEncoder = rightMinorMotor.getEncoder();
+
+		// motors to invert
+		leftMajorMotor.setInverted(true);
+
+		// set current limits
+		rightMajorMotor.setSmartCurrentLimit(30);
+		leftMajorMotor.setSmartCurrentLimit(30);
+		rightMinorMotor.setSmartCurrentLimit(30);
+		leftMinorMotor.setSmartCurrentLimit(30);
+
+		// set PID coefficients
+		majorPIDController.setP(ArmConstants.kMajorArmP);
+		majorPIDController.setI(ArmConstants.kMajorArmI);
+		majorPIDController.setD(ArmConstants.kMajorArmD);
+		majorPIDController.setIZone(0);
+		majorPIDController.setFF(0);
+		majorPIDController.setOutputRange(-1, 1);
+
+		// set PID coefficients
+		minorPIDController.setP(ArmConstants.kMinorArmP);
+		minorPIDController.setI(ArmConstants.kMinorArmI);
+		minorPIDController.setD(ArmConstants.kMinorArmD);
+		minorPIDController.setIZone(0);
+		minorPIDController.setFF(0);
+		minorPIDController.setOutputRange(-1, 1);
+
+		leftMajorMotor.follow(rightMajorMotor);
+		leftMinorMotor.follow(rightMinorMotor);
 	}
 
 	@Override
