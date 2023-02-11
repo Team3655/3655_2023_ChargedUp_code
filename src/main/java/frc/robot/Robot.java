baf86207591b920e863cc3f7fb6b0fddefa55444
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2023-02-11 12:16:09 -0700

    THE PURGE OF THE M_

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,9 +18,9 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-	private Command m_autonomousCommand;
+	private Command autonomousCommand;
 
-	private RobotContainer m_robotContainer;
+	private RobotContainer robotContainer;
 	// private ArmSubsystem m_ArmSubsystem;
 
 	/**
@@ -33,7 +33,7 @@ public class Robot extends TimedRobot {
 		// Instantiate our RobotContainer. This will perform all our button bindings,
 		// and put our
 		// autonomous chooser on the dashboard.
-		m_robotContainer = new RobotContainer();
+		robotContainer = new RobotContainer();
 		// m_ArmSubsystem = new ArmSubsystem();
 	}
 
@@ -70,11 +70,11 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+		autonomousCommand = robotContainer.getAutonomousCommand();
 
 		// schedule the autonomous command (example)
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.schedule();
+		if (autonomousCommand != null) {
+			autonomousCommand.schedule();
 		}
 
 	}
@@ -91,8 +91,8 @@ public class Robot extends TimedRobot {
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
+		if (autonomousCommand != null) {
+			autonomousCommand.cancel();
 		}
 
 	}
